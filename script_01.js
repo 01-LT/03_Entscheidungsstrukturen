42c851ba84bd3eb9d1df2f347bb50dd096ba8fad
Lawrence <lawrence.langen@gmail.com>
2023-01-17 08:52:45 +0100

    Fallunterscheidung / SWITCH|CASE 2

--- script_01.js
+++ script_01.js
@@ -83,29 +83,46 @@ let job;
 // job = "teacher";    // .. unterrichtet!
 job = "instructor";  // .. unterrichtet!
 
-switch (job) 
-{
-    case "driver":
-        console.log("Jane fährt TAXI!");
-        break;
-    case "diver":
-        console.log("Jane taucht im Rhein!");
-        break;
-    case "artist":
-        console.log("Jane malt ein Bild!");
-        break;
-    case "teacher":
-    case "instructor":
-        console.log("Jane unterrichtet!");
-        break;
+// switch (job) 
+// {
+//     case "driver":
+//         console.log("Jane fährt TAXI!");
+//         break;
+//     case "diver":
+//         console.log("Jane taucht im Rhein!");
+//         break;
+//     case "artist":
+//         console.log("Jane malt ein Bild!");
+//         break;
+//     case "teacher":
+//     case "instructor":
+//         console.log("Jane unterrichtet!");
+//         break;
 
-    // case "instructor" || "teacher": // ??
-    //     console.log("Jane unterrichtet!");
-    //     break;
+//     // case "instructor" || "teacher": // ??
+//     //     console.log("Jane unterrichtet!");
+//     //     break;
 
 
+//     default:
+//         console.log("Jane macht etwas anderes!");
+//         break;
+// }
+
+/********  Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const a = 10;
+
+switch (cond) {
+    case (a == 1): // true && true
+        console.log("a ist gleich 1");
+        break;
+    case (a == 2):
+        console.log("a ist gleich 2");
+        break;
     default:
         console.log("Jane macht etwas anderes!");
+        console.log("a hat einen anderen Wert");
         break;
 }
-
